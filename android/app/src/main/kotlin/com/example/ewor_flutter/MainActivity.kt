f48d34b752d72abb081a2ba7789fd9982bf8e515
TONNY BAWEMBYE <tonnybaw75@gmail.com>
2022-12-26 23:38:11 +0300

    -removed irrelevant platforms -added firebase to configure hosting

--- android/app/src/main/kotlin/com/example/ewor_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ewor_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:ewor_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'landing/index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
